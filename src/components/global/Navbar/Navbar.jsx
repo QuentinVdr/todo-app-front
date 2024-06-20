813ebd35955d10da82d003efa8498c4cea20b402
Quentin VERDIER <quent1.verdier@gmail.com>
2024-06-20 20:16:13 +0200

    ✨ Rename projet to "Todo-app"

--- src/components/global/Navbar/Navbar.jsx
+++ src/components/global/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
     <AppBar className={styles.navbar} position="sticky">
       <Stack direction="row" gap={2} alignItems="center">
         <Link to="/" className={styles.logo}>
-          <Typography variant="h1">my-app</Typography>
+          <Typography variant="h1">Todo-app</Typography>
         </Link>
         <div className={styles.userMenu}>
           <IconButton
