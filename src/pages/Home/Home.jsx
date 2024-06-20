0f116f21aab50969c535de116aa723feeff58ffe
Quentin VERDIER <quent1.verdier@gmail.com>
2024-06-20 20:14:19 +0200

    ✨ Add task creation modal to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,35 @@
-import { Grid, Typography } from '@mui/material';
+import { TaskForm } from '@components/task/TaskFrom/TaskForm';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Grid, IconButton, Modal, Typography } from '@mui/material';
+import { useState } from 'react';
+import styles from './Home.module.scss';
 
 /**
  * Page that contains all the components displayed on the application homepage
  */
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const onFormSubmit = (data) => {
+    console.log(data);
+    handleClose();
+  };
+
   return (
-    <Grid>
+    <Grid container direction="row" justifyContent="space-between">
       <Typography variant="h1">Bienvenue</Typography>
+      <IconButton onClick={handleOpen} size="large">
+        <AddCircleOutlineIcon />
+      </IconButton>
+      <Modal open={open} onClose={handleClose}>
+        <Grid container gap={4} className={styles.modalBox}>
+          <Typography variant="h2">Créer une tache</Typography>
+          <TaskForm onSubmit={onFormSubmit} onCancel={() => handleClose()} />
+        </Grid>
+      </Modal>
     </Grid>
   );
 }
