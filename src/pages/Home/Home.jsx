1d2cb90d546afd85ce6f46e908e0899828ffc1a1
Quentin Verdier <quent1.verdier@gmail.com>
2024-06-21 09:19:48 +0200

    ✨ Add get all task and display

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import { TaskForm } from '@components/task/TaskFrom/TaskForm';
+import { useTagQuery } from '@hooks/reactQuery/queries/useTagQueries';
+import { useTaskQuery } from '@hooks/reactQuery/queries/useTaskQueries';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { Grid, IconButton, Modal, Typography } from '@mui/material';
+import { CircularProgress, Grid, IconButton, Modal, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
 import styles from './Home.module.scss';
 
@@ -8,6 +10,9 @@ import styles from './Home.module.scss';
  * Page that contains all the components displayed on the application homepage
  */
 export default function Home() {
+  const { data: tags } = useTagQuery();
+  const { data: tasks, isLoading: isTasksLoading } = useTaskQuery();
+
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => setOpen(true);
@@ -18,18 +23,59 @@ export default function Home() {
     handleClose();
   };
 
+  const findTagNameById = (id) => {
+    return tags.find((tag) => tag._id === id)?.name;
+  };
+
   return (
-    <Grid container direction="row" justifyContent="space-between">
-      <Typography variant="h1">Bienvenue</Typography>
-      <IconButton onClick={handleOpen} size="large">
-        <AddCircleOutlineIcon />
-      </IconButton>
+    <>
+      <Grid container direction="column" rowGap={6}>
+        <Grid item container direction="row" justifyContent="space-between">
+          <Typography variant="h1">Bienvenue</Typography>
+          <IconButton onClick={handleOpen} size="large">
+            <AddCircleOutlineIcon />
+          </IconButton>
+        </Grid>
+        {isTasksLoading ? (
+          <Typography variant="body1">
+            Chargement <CircularProgress />
+          </Typography>
+        ) : (
+          <Grid item container rowGap={4} columnSpacing={4}>
+            {tasks.map((task) => (
+              <Grid
+                item
+                xs={12}
+                key={task._id}
+                container
+                direction="row"
+                justifyContent="space-between"
+                className={styles.card}
+              >
+                <Stack>
+                  <Typography variant="h2">{task.title}</Typography>
+                  <Typography variant="body1">{task.description}</Typography>
+                </Stack>
+                {task.tags.length > 0 && (
+                  <Stack>
+                    {task.tags.map((taskTag) => (
+                      <Typography key={taskTag} variant="body2">
+                        {findTagNameById(taskTag)}
+                      </Typography>
+                    ))}
+                  </Stack>
+                )}
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Grid>
       <Modal open={open} onClose={handleClose}>
-        <Grid container gap={4} className={styles.modalBox}>
+        <Grid container direction="column" rowGap={4} className={styles.modalBox}>
           <Typography variant="h2">Créer une tache</Typography>
           <TaskForm onSubmit={onFormSubmit} onCancel={() => handleClose()} />
         </Grid>
       </Modal>
-    </Grid>
+    </>
   );
 }
